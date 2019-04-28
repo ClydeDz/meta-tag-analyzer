01dcb279b58f0b76de849b36bbce50843c2ce258
Clyde D'Souza <clyd94@gmail.com>
2019-04-28 23:03:58 +1200

    Remove checked-in js files

--- Src/buildAndReleaseTask/tests/success.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tmrm = require("azure-pipelines-task-lib/mock-run");
-const path = require("path");
-let taskPath = path.join(__dirname, '..', 'index.js');
-let tmr = new tmrm.TaskMockRunner(taskPath);
-tmr.setInput('sitemapURL', 'https://raw.githubusercontent.com/ClydeDz/meta-tag-analyzer/master/Sample/sitemap.xml');
-tmr.run();
